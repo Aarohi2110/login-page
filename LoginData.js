68e9622a70067af138bc54b77e9fde4440b32d24
Aarohi2110 <82254142+Aarohi2110@users.noreply.github.com>
2022-04-16 01:59:46 -0700

    Create LoginData.js

---
+++ LoginData.js
@@ -0,0 +1,18 @@
+export const questions = [
+  {
+    id: 1,
+    question: "Create New Account ",
+    answer: "yes",
+  },
+  {
+    id: 2,
+    question: "Login as a Donor",
+    answer: "yes",
+  },
+  {
+    id: 3,
+    question: "Login as a Receiver",
+    answer: "yes",
+  },
+  
+];
